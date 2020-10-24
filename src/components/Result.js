086afae300309611401fb71adda8a51381b13a8a
Antek <antek.gawron@gmail.com>
2020-10-24 21:13:27 +0200

    add loose/gain per day/week

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,18 +6,27 @@ const Result = (props) => {
   const getDateDifference = (dateFirst, dateSecond) => {
     const firstDate = new Date(dateFirst).getTime();
     const secondDate = new Date(dateSecond).getTime();
-    console.log((secondDate - firstDate) / 1000)
+    // console.log((secondDate - firstDate) / 86400000)
+    return (secondDate - firstDate) / 86400000
   }
   const getBMI = (weight, height) => {
     const BMI = (weight / Math.pow((height / 100), 2)).toFixed(1)
-    console.log(weight, height)
+    // console.log(weight, height)
     return BMI;
   }
+  const changePerDay = (weight, days) => {
+    // console.log("ok")
+    return (weight / days).toFixed(2)
+  }
+  const result = initWeight - desiredWeight > 0 ? "loose" : "gain";
+
   return (
     <div>
-      <p>You want {initWeight - desiredWeight > 0 ? "loose" : "gain"} {Math.abs(initWeight - desiredWeight)}kg</p>
+      <p>You want {result} {Math.abs(initWeight - desiredWeight)}kg</p>
       <p>Your current BMI is {getBMI(initWeight, height)}</p>
       <p>Your desired BMI is {getBMI(desiredWeight, height)}</p>
+      <p>You should {result} {changePerDay(Math.abs(initWeight - desiredWeight), getDateDifference(startDate, endDate))} kg per day</p>
+      {getDateDifference(startDate, endDate) >= 7 ? <p>You should {result} {changePerDay(Math.abs(initWeight - desiredWeight), (getDateDifference(startDate, endDate)) / 7)} kg per week</p> : null}
     </div>
   );
 }
