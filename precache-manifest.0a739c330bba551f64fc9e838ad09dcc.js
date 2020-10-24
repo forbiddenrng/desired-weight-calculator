d5136ea1ff0172c6e059f593e0dcdd3c426657d1
Antek <antek.gawron@gmail.com>
2020-10-24 22:02:08 +0200

    Updates

---
+++ precache-manifest.0a739c330bba551f64fc9e838ad09dcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54b3a505cac186100f82f595f638873",
+    "url": "/desired-weight-calculator/index.html"
+  },
+  {
+    "revision": "aafbee2cf1bbc3fde279",
+    "url": "/desired-weight-calculator/static/css/main.e3696972.chunk.css"
+  },
+  {
+    "revision": "da9c1755ed938adca8de",
+    "url": "/desired-weight-calculator/static/js/2.87c4ae9e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/desired-weight-calculator/static/js/2.87c4ae9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aafbee2cf1bbc3fde279",
+    "url": "/desired-weight-calculator/static/js/main.92e7bc7f.chunk.js"
+  },
+  {
+    "revision": "2fc82045914684b22bd4",
+    "url": "/desired-weight-calculator/static/js/runtime-main.f16c5cb5.js"
+  }
+]);
